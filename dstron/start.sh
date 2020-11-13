e25068b831f9c6fea145b246dc78e7026fd15e8a
eosjust <eosjust@outlook.com>
2020-11-13 10:54:17 +0800

    更新自启脚本

--- dstron/start.sh
+++ dstron/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #chkconfig:2345 80 90
 #decription:autostart
-sleep 30s
+export JAVA_HOME=/opt/java/jdk1.8.0_271
+export PATH=$JAVA_HOME/bin:$PATH
 nohup java -jar ~/atktron/dstron/target/dstron.jar >> ~/dstron.log 2>&1 &
